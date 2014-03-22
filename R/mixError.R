c24c7977547f7178005810441be5b43231413c83
Lingbing Feng <fenglb88@gmail.com>
2014-03-22 23:56:42 +1100

    a new function to calculate mixed error

---
+++ R/mixError.R
@@ -0,0 +1,21 @@
+#' Calculate mixed error when the imputed matrix is mixed type
+#' 
+#' @param imp the imputed matrix
+#' @param mis the original matrix with missing values
+#' @param true the true matrix
+#' @param norm logical, if TRUE, the nomailised RMSE will return for continous variables
+#' @export
+mixError <- function(imp, mis, true, norm = TRUE) {
+  Type <- Detect(imp)
+  err <- rep(NA, 2)
+  names(err) <- c('RMSE', 'MCE')
+  for (t.type in Type) {
+    t.ind <- which(Type == t.type)
+    if (t.type == "numeric"){
+      err[1] <- Rmse(imp[,t.ind], mis[,t.ind], true[,t.ind], norm = norm)
+    } else {
+      err[2] <- mr(imp[,t.ind], mis[,t.ind], true[,t.ind])
+    }
+  }
+  return(err)
+}
