848ddb9d2c9d36511a1c8bc5ef5edf1154e44610
SteffenMoritz <steffen.moritz10@gmail.com>
2019-07-01 16:04:32 +0200

    Logo

---
+++ man/figures/createLogo.R
@@ -0,0 +1,6 @@
+library("hexSticker")
+
+p <- "picture.png"
+
+sticker(p, package="imputeR", p_family ="Arial", p_color = "#FFFFFF", h_fill = "#0F80FF", h_color ="#878787", p_size=8, s_x=1.1, s_y=.80, s_width=0.80, s_height=1,
+        filename="imputeR-logo2.png")
